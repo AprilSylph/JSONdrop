aaa08dbf753247ff18cb8fd4bdbabe7554204b2f
April Sylph <april@sylph.tech>
2019-06-04 17:05:34 +0100

    preview objects passed as url parameters

--- preview/preview.js
+++ preview/preview.js
@@ -1,1 +1,13 @@
-const preview = value => document.body.innerHTML = render(value);
+const preview = (value, config) => document.body.innerHTML = render(value, config);
+
+window.addEventListener("load", () => {
+	try {
+		let params = (new URL(location)).searchParams;
+		let object = params.get("object");
+		if (object !== null) {
+			preview(JSON.parse(object), JSON.parse(params.get("config")));
+		}
+	} catch (error) {
+		preview({"error": error}, {autoOpenDepth: 1});
+	}
+});
