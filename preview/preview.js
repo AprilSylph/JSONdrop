53327ac6aecc1e8ba565c252b3727e50545b039c
April Sylph <april@sylph.tech>
2020-04-19 01:41:13 +0100

    rewrite (#4)

--- preview/preview.js
+++ preview/preview.js
@@ -1,13 +1,17 @@
-const preview = (value, config) => document.body.innerHTML = render(value, config);
+/* globals convert */
+const preview = (value, config) => {
+  document.body.innerHTML = '';
+  document.body.appendChild(convert(value, config));
+};
 
-window.addEventListener("load", () => {
-	try {
-		let params = (new URL(location)).searchParams;
-		let object = params.get("object");
-		if (object !== null) {
-			preview(JSON.parse(object), JSON.parse(params.get("config")));
-		}
-	} catch (error) {
-		preview({"error": error}, {autoOpenDepth: 1});
-	}
+window.addEventListener('load', () => {
+  try {
+    const params = (new URL(location)).searchParams;
+    const object = params.get('object');
+    if (object !== null) {
+      preview(JSON.parse(object), JSON.parse(params.get('config')));
+    }
+  } catch (error) {
+    preview({error: error.toString()}, {autoOpenDepth: 1});
+  }
 });
